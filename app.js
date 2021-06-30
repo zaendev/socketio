75f5a116b1f73a86541bd0be28049bdae3116281
zain <31380073+zainstar@users.noreply.github.com>
2021-07-01 02:01:58 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// const io = require("socket.io")({
+//     // path: "/test",
+//     serveClient: false,
+//   });
+
+app.use(express.static('public'))
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*'
+    }
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    // in a listener
+    socket.on("message", (data) => {
+        socket.emit('message', data)
+    });
+});
+
+server.listen(3003);
